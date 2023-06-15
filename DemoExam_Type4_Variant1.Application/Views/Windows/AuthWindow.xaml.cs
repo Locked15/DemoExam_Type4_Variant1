2b2f1f933f5f4f39c53d00f7f73bc05e61aecd21
Locked15 <almgal353@gmail.com>
2023-06-15 22:02:36 +0500

    Authorization is completed.

--- DemoExam_Type4_Variant1.Application/Views/Windows/AuthWindow.xaml.cs
+++ DemoExam_Type4_Variant1.Application/Views/Windows/AuthWindow.xaml.cs
@@ -1,15 +1,38 @@
-﻿using System.Windows;
+﻿using DemoExam_Type4_Variant1.Application.Common;
+using System.Windows;
 
 namespace DemoExam_Type4_Variant1.Application.Views.Windows
 {
     /// <summary>
-    /// Interaction logic for AuthWindow.xaml
+    /// Interaction logic for AuthWindow.xaml.
     /// </summary>
     public partial class AuthWindow : Window
     {
         public AuthWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnLogInButtonClick(object sender, RoutedEventArgs e)
+        {
+            var login = userLoginInput.Text;
+            var password = userPasswordInput.Password;
+
+            if (SessionData.TryToContinueAsUser(login, password))
+                OpenProductsWindow();
+            else
+                MessageBox.Show("Аккаунт с указанными данными не найден.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void OnContinueAsGuestButtonClick(object sender, RoutedEventArgs e)
+        {
+            SessionData.ContinueAsGuest();
+            OpenProductsWindow();
+        }
+
+        private void OpenProductsWindow()
+        {
+            Close();
         }
     }
 }
