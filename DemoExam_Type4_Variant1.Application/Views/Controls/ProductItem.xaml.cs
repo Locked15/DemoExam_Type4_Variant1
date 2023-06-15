6d932874bc4531c2005bb0c0c3dfdc89e5e678a2
Locked15 <almgal353@gmail.com>
2023-06-15 23:41:59 +0500

    Added controller for Products Window ListView.

---
+++ DemoExam_Type4_Variant1.Application/Views/Controls/ProductItem.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DemoExam_Type4_Variant1.Application.Common;
+using DemoExam_Type4_Variant1.Application.Models.Entities;
+using DemoExam_Type4_Variant1.Application.Views.Windows;
+using System.Windows.Controls;
+
+namespace DemoExam_Type4_Variant1.Application.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for ProductItem.xaml.
+    /// </summary>
+    public partial class ProductItem : UserControl
+    {
+        private readonly ProductsWindow _parentWindow;
+
+        public Product Model { get; set; }
+
+        public ProductItem(ProductsWindow parentWindow, Product model)
+        {
+            _parentWindow = parentWindow;
+            Model = model;
+
+            InitializeComponent();
+            DataContext = Model;
+        }
+
+        private void OnAddToOrderClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            SessionData.CurrentOrder?.AddNewProductToOrder(Model);
+            _parentWindow.UpdateOrderNavigationVisibility();
+        }
+    }
+}
