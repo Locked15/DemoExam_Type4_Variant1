c8ff37a71e7cbc148e49c303bf068c3254fb277e
Locked15 <almgal353@gmail.com>
2023-06-15 21:44:58 +0500

    Added functionality to work with session data.

--- DemoExam_Type4_Variant1.Application/Common/SessionData.cs
+++ DemoExam_Type4_Variant1.Application/Common/SessionData.cs
@@ -1,6 +1,53 @@
-﻿namespace DemoExam_Type4_Variant1.Application.Common
+﻿using DemoExam_Type4_Variant1.Application.Models.Entities;
+using System;
+using System.Linq;
+
+namespace DemoExam_Type4_Variant1.Application.Common
 {
-    internal class SessionData
+    public static class SessionData
     {
+        public static User? CurrentUser { get; private set; }
+
+        public static Order? CurrentOrder { get; private set; }
+
+        public static bool TryToContinueAsUser(string login, string password)
+        {
+            var user = DemoExamDataContext.Instance.Users.FirstOrDefault(user =>
+                                                                         user.Login == login &&
+                                                                         user.Password == password);
+            if (user == null)
+            {
+                return false;
+            }
+            else
+            {
+                CurrentUser = user;
+                GenerateNewOrder();
+
+                return true;
+            }
+        }
+
+        public static void ContinueAsGuest()
+        {
+            CurrentUser = null;
+            GenerateNewOrder();
+        }
+
+        public static void LogOutFromAccount()
+        {
+            CurrentUser = null;
+            CurrentOrder = null;
+        }
+
+        public static void GenerateNewOrder()
+        {
+            CurrentOrder = new Order()
+            {
+                UserId = CurrentUser?.Id,
+                StatusId = Status.DefaultStatusId,
+                TakeCode = new Random().Next(100, 1000)
+            };
+        }
     }
 }
